94e9429911c6ea6896e82598748106d81c2b7499
Hamm-JH <rimmoyee@gmail.com>
2022-10-16 22:40:59 +0900

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -1,10 +1,15 @@
 import math
+import time
+
+import requests
 
 import cv2
 import pyrealsense2
 from realsense_depth import *
 
 dc = DepthCamera()
+XPPerD = 0.1359375  # 픽셀당 x 각도
+YPPerD = 0.11875    # 픽셀당 y 각도
 
 # region [1. parse]
 
@@ -59,6 +64,87 @@ def GetNearTwo(peoples: list, center):
 
 #endregion
 
+# region [3. 리얼벡터 구하기]
+
+# region [3-1. 픽셀차에서 각도 구하기]
+
+def distToDeg(xdist, ydist):
+    """ 각도 구하기"""
+    xdeg = xdist * XPPerD
+    ydeg = ydist * YPPerD
+
+    return (xdeg, ydeg)
+
+# endregion
+
+# region [3-2. 3-6. 각도로 평행거리 구하기]
+
+def degToHDist(deg, cDepth):
+    """ 각도로 거리구하기"""
+    # print(deg)
+
+    xhdist = math.tan(math.radians(deg[0])) * cDepth
+    yhdist = math.tan(math.radians(deg[1])) * cDepth
+
+    # print(xhdist, yhdist)
+    return (xhdist, yhdist)
+
+# endregion
+
+# region [3-3. 평행 거리 구하기]
+
+def horizontalDepth(hdist, cDepth):
+    """ 포인트의 평행 뎁스 구하기"""
+    t1 = hdist[0]
+    t2 = hdist[1]
+    t3 = float(cDepth)
+
+    hDepth = math.sqrt(t1**2 + t2**2 + t3**2)
+
+    return hDepth
+
+# endregion
+
+# region [3-4. 평행뎁스와 측정 뎁스간 비율 구하기]
+
+def depthRatio(hdepth, depth):
+    """ 수평거리 뎁스와 그냥 뎁스간 비교"""
+    
+    # 측정된 거리 / 수평 뎁스거리
+    return depth / hdepth
+
+# endregion
+
+# region [3-5. 측정된 비율로 측정 뎁스의 중심 뎁스 구하기]
+
+def getCenterDepth(dRatio, cDepth):
+    """ 측정 거리의 중심 뎁스 계산"""
+
+    return cDepth * dRatio
+
+# endregion
+
+# region [3-7. 중심점에서 뎁스간 사잇거리 계산]
+
+def diffCenterDepth(rcDepth, cDepth):
+    """ 중심점간 거리 구하기"""
+    return rcDepth - cDepth
+
+# endregion
+
+# endregion
+
+# region [4. 벡터간 거리 구하기]
+
+def getFinalDists(p1, p2):
+    """ 두 사람간 거리 계산"""
+    p1c = float(p1[2])
+    p2c = float(p2[2])
+    p1p2 = math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
+
+    return p1c, p2c, p1p2
+
+# endregion
 
 # list :: list
 # camIndex :: str
@@ -74,8 +160,11 @@ def OnDetect(image, list : list[str], camIndex : str):
     # dc = DepthCamera()
     ret, depth_frame, color_frame = dc.get_frame()
 
+    # 중심 뎁스 계산
     cDep = depth_frame[320, 240]
-    print(f'distance {cDep}')
+    # 중심 좌표
+    cPoint = (320, 240, cDep)
+    # print(f'center distance {cDep}')
 
     peoples = []
 
@@ -102,17 +191,90 @@ def OnDetect(image, list : list[str], camIndex : str):
 
             peoples.append(people)
 
-
-
     if len(peoples) >= 2:
-        nears = GetNearTwo(peoples, cPx)
+        realVector = []
 
-        print(nears)
-        # if len(peoples) > 2:
-        #     peoples = GetNearTwo(peoples)
+        # 근처 두명 추려내기 (캠 기준)
+        nears = GetNearTwo(peoples, cPx)
+        # print(nears)
+
+        cv2.line(image, (nears[0][0], nears[0][1]), (nears[1][0], nears[1][1]), (255, 0, 255))
 
         # 근처 두명 잡아두고 거리재기 시작
+        # x축 fov 87
+        # y축 fov 57
+        for elem in nears:
+            # 0 [400, 327, 347]
+            # 1 [112, 384, 1355]
+            # print(f'center depth {cDep}')
+            # print(f'element {elem}')
 
+            xdiff = elem[0] - cPoint[0] # x 픽셀차
+            ydiff = elem[1] - cPoint[1] # y 픽셀차
+            xdist = int(math.fabs(xdiff))
+            ydist = int(math.fabs(ydiff))
 
-    cv2.circle(image, (320, 240), 15, (255, 0, 255), -1)
+            # print(xdiff, ydiff)
+            # print(xdist, ydist)
+            
+            # 1. 픽셀차로 각도 구하기
+            deg = distToDeg(xdist, ydist)
+            # print(res)
+
+            # 2. 구한 각도로 평행 거리 구하기
+            hdist = degToHDist(deg, cDep)
+            # print(f'h dist : {hdist}')
+
+            # 3. 구한 거리로 평행 뎁스 구함
+            hdepth = horizontalDepth(hdist, cDep)
+            # print(f'h depth : {hdepth}')
+
+            # 4. 측정된 거리 / 수평 뎁스거리 계산
+            # 1보다 크면 카메라 기준점보다 멀리 있음
+            # 1보다 작으면 카메라 기준점보다 가까이 있음
+            depRatio = depthRatio(hdepth, elem[2])
+            # print(f'depth ratio : {depRatio}')
+
+            # 5. 측정된 비율로 측정 뎁스의 중심뎁스 계산
+            calCenterDepth = getCenterDepth(depRatio, cDep)
+            # print(f'cal center depth : {calCenterDepth}')
+
+            # 6. 각도와 실측 중심 뎁스간 평행 거리 구하기
+            rcdist = degToHDist(deg, calCenterDepth)
+            # print(f'real center to point {rcdist}')
+
+            # 7. 중심점에서 뎁스간 사잇거리 계산
+            rcdDiff = diffCenterDepth(calCenterDepth, cDep)
+            # print(f'center diff {rcdDiff}')
+
+            # 8. 최종 중심점 대비 리얼벡터
+            result = (rcdist[0], rcdist[1], rcdDiff)
+            # print(f'result {result}')
+
+            realVector.append(result)
+
+        p1c, p2c, p1p2 = getFinalDists(realVector[0], realVector[1])
+
+        data = {
+            "camID" : camIndex,
+            "result" : {
+                "p1CDist" : math.fabs(p1c),
+                "p2CDist" : math.fabs(p2c),
+                "p1p2Dist" : p1p2
+            }
+        }
+
+        # print(type(data))
+        # print(type(camIndex))
+        # print(type(p1c))
+        # print(type(p2c))
+        # print(type(p1p2))
+
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+        # params=
+        res = requests.get(url='http://192.168.10.11:3002/VISION', json=data, headers=headers)
+
+        time.sleep(0.1)
+
+    # cv2.circle(image, (320, 240), 15, (255, 0, 255), -1)
 
